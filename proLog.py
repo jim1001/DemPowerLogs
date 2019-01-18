746ec9bd6e181482a415f3509ebce44b85591fe2
James Schumm <j.schumm@manchester.ac.uk>
2019-01-18 12:35:16 +0000

    Summary: Add code to iterate through all files in coded directory
    Changes:
    Produce an output file for all files in given input directory
    
    Other notes:
    
    Tested:
    Yes - checked same number of output files as input files and spot-checked output file 3_8_2018.txt.

--- proLog.py
+++ proLog.py
@@ -1,14 +1,32 @@
 import re
 import csv
-f = open("UsageLogs/In/26_6_2018.txt", "r") # Open file for reading
-f_out = open("UsageLogs/Out/26_6_2018_out.txt", "w",encoding='utf-8') # Open file for (over-)writing
-reader = csv.reader(f)
-for row in reader:
-    screen = row[0] # Put first element of log line into vble "screen"
-    if screen[3:5] == "_3": # _3 indicates activity screen
-        section = screen[0:3] # save section number
-    datetime = row[1] # Put second element of log line into vble "datetime"
-    if "Photo" in screen or "TextRes" in screen:
-        screen = section + "_" + screen # Prepend section number
-    f_out.write(screen + "," + datetime +  "\r")
-f.close()                   # Free up system resources.
+import os
+
+path = 'UsageLogs/In'
+folder = os.fsencode(path)
+filenames = []
+
+for file in os.listdir(folder):
+    filename = os.fsdecode(file)
+    #if filename.endswith( ('.jpeg', '.png', '.gif') ): # whatever file types you're using...
+    filenames.append(filename)
+
+filenames.sort() #sorts in ascending order
+for file in filenames:
+	print("Processing " + file + "\n")
+	pathIn = "UsageLogs/In/" + file
+	pathOut = "UsageLogs/Out/" + file
+	f_in = open(pathIn, "r")
+	f_out = open(pathOut, "w")
+	reader = csv.reader(f_in)
+	
+	for row in reader:
+		screen = row[0] # Put first element of log line into vble "screen"
+		if screen[3:5] == "_3": # _3 indicates activity screen
+			section = screen[0:3] # save section number
+		datetime = row[1] # Put second element of log line into vble "datetime"
+		if "Photo" in screen or "TextRes" in screen:
+			screen = section + "_" + screen # Prepend section number
+		f_out.write(screen + "," + datetime +  "\r")
+	f_in.close()
+	f_out.close()
